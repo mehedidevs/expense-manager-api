736a0ea3aaea5d4154a3aefdca44a53cbad221c1
masum_devs <mehedieasital@gmail.com>
2024-03-05 12:18:28 +0600

    initial commit

---
+++ src/test/java/com/mehedi/exapense/manager/ExapenseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehedi.exapense.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExapenseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
